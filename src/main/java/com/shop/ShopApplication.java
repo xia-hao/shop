95841db6a33279e4b9a6ccc09e4c9ffcfb2579f2
xia-hao <18188907492@163.com>
2020-02-28 20:38:03 +0800

    启动类添加ServletComponentScan注解

--- src/main/java/com/shop/ShopApplication.java
+++ src/main/java/com/shop/ShopApplication.java
@@ -3,10 +3,12 @@ package com.shop;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 import java.text.ParseException;
 
 @SpringBootApplication
+@ServletComponentScan
 @MapperScan("com.shop.mapper")
 public class ShopApplication {
 
